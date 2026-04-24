ca0a82e0426854d318f565720e87f411479c81d9
LeftFrog <oleonov.leftfrog@gmail.com>
2026-04-23 20:59:07 -0600

    Added new api point for auth for server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import db from './db.js';
+import { verifyToken } from './auth.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
+
+app.use('/api', verifyToken);
 
 // === RACK SKIDS ===
 

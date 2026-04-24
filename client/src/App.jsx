ef44d5f89c6168f32ad92015e4e90ec7333bbdea
LeftFrog <oleonov.leftfrog@gmail.com>
2026-04-23 20:15:32 -0600

    Added auth to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import { act, useState } from "react"
+import { act, useEffect, useState } from "react"
 import Layout from "./components/Layout"
 import Inventory from "./modules/Inventory"
 import "./styles.css"
+import { onAuthStateChanged, signInWithEmailAndPassword, signOut} from "firebase/auth"
+import { auth } from "./firebase.js"
 
 export default function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    return onAuthStateChanged(auth, (u) => {
+      setUser(u);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) return <div className="loading">Loading...</div>;
+  if (!user) return <LoginForm />;
+
   const [activeModule, setActiveModule] = useState('inventory');
 
   return (<Layout activeModule={activeModule} onModuleChange={setActiveModule}>
